d358ceae7f3d4e01c79e7f8fa333d8de7241182f
someone735 <smeone83@gmail.com>
2025-02-23 19:24:56 -0700

    created guest user constructor

--- backend/GuestUser.py
+++ backend/GuestUser.py
@@ -1,4 +1,5 @@
 from User import User
+from User import inputCredentials
 from Signup import createRegisteredUser
 
 
@@ -9,13 +10,16 @@ class GuestUser:
         self.UserBase = User(email, password)
         
 
-def createGuestUser():
-    # create a guest user 
-    
-    return 0
+def createGuestUser(email, password): # used by guest login to create guest user
+    # GuestUser constructor
+    newGuest = GuestUser(email, password)
+    return newGuest
+
+
 
 def sendGuestToSignup(guestUserTransport):
     # send guest data to signup to create a registered user
+    
     guestTransport = {}
     guestTransport["email"] = guestUserTransport.email
     guestTransport["password"] = guestUserTransport.password
