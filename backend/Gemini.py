5790f8430f052b73188e0cd5b35e90896c4c125b
Altinb <altin_15.2004@hotmail.com>
2025-03-15 10:49:21 -0600

    edit ai method

--- backend/Gemini.py
+++ backend/Gemini.py
@@ -1,14 +1,13 @@
 from google import genai
 
 
-def getResponse():
+def getResponse(message):
     client = genai.Client(api_key="AIzaSyCpnYk7EshXUwbqK_PBp7izaPVrd5wK1q8")
+    content = "Using the given text, make a recipe for a meal. If this text that is given does not have anything to do with making recipes then say so. Here is the given text: " + message
 
     response = client.models.generate_content(
         model="gemini-2.0-flash", 
-        contents="Using the given text, make a recipe for a meal. If this text that is given does not have anything to do with making recipes then say so. Here is the given text: I have chicken and rice"
+        contents=content
     )
 
     return response.text
-
-print(getResponse())
