331fda587ac973dbb4fb14a99c7951c168790ff8
The Chosen One <144161398+IvanCase@users.noreply.github.com>
2025-03-20 17:20:11 -0600

    Create footer.js

---
+++ frontend/src/views/footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-left">
+      <span className="footer-text">© 2025 White Dressing. All rights reserved.</span>
+      </div>
+    <div className="footer-center">
+      <div className="footer-item">
+      About Us
+      <span className="tooltip"> SENG401 G14 <br></br> Mark Jimenez <br></br> Ivan Ibe <br></br> 
+      Mark Guerta <br></br> Altin Bakiu <br></br> Danial Farrukh <br></br> John Tumacder</span>
+      </div>
+      <div className="footer-item">
+      Contact
+      <span className="tooltip">White.D@gmail.com <br></br> (403)-123-4567 </span>
+      </div>
+    </div>
+    <div className="footer-right"> 
+    <img src="/external/Logo3.png" className="footer-logo"/>
+    </div> 
+  </footer>
+  );
+};
+
+export default Footer;
