227df3d9e41def1a5e45b2a4cb62a17a59451644
Mark Jimenez <mark.jimenez@ucalgary.ca>
2025-03-20 11:37:46 -0600

    Updated Nav-bar

--- frontend/src/views/nav-bar.js
+++ frontend/src/views/nav-bar.js
@@ -13,7 +13,7 @@ const NavBar = () => {
   };
 
   const handleSignOut = () => {
-    localStorage.removeItem("isGuest");
+    localStorage.setItem("isGuest","True");
     navigateTo('/login');
   };
 
